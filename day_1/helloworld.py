d68f7b77487bf811b76f6ce9362f2548d1e489f8
Matthew Lopez <matthew.is.riding@gmail.com>
2023-03-18 15:50:39 -0700

    Basic refresh

---
+++ day_1/helloworld.py
@@ -0,0 +1,18 @@
+print(3+4)
+print(4-3)
+print(4*3)
+print(4%3)
+print(4/3)
+print(3**4)
+print(4//3)
+
+print("Matt")
+print("Lopez")
+print("California")
+
+print(type(10))
+print(type(9.8))
+print(type(3.14))
+print(type(4 - 4j))
+print(type(['Asabeneh', 'Python', 'Finland']))
+print(type("Matt"))
---
+++ day_2/variables.py
@@ -0,0 +1,26 @@
+# Day 2
+first_name = "Matt"
+last_name = "Lopez"
+full_name = f"{first_name} {last_name}"
+
+this, that, the_other = "this", "that", "other"
+
+print(type(first_name))
+print(type(last_name))
+print(type(full_name))
+
+num_1 = 24
+num_2 = 54
+
+summed = num_1 + num_2
+diff = num_2 - num_1
+
+name_diff = len(last_name) - len(first_name)
+
+pi = 3.14
+rad = input("Enter a radius (integer): ")
+circle_area = pi * (rad * rad)
+circle_circumference = 2 * (pi * rad)
+
+print(circle_area)
+print(circle_circumference)
